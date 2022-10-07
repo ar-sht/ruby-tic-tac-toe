96f0ffef171b65c7bf4af04997e9ee99f39a3e9c
Ari Shtein <ashtein120@gmail.com>
2022-10-07 12:46:39 -0400

    Add Board tests

--- lib/board.rb
+++ lib/board.rb
@@ -8,32 +8,32 @@ class Board
     @squares = squares
   end
 
-  def mark_square(x_coord, y_coord, player)
-    @squares[2 - y_coord][x_coord] = player.zero? ? 'X' : 'O'
-  end
-
-  def get_square(x_coord, y_coord)
-    @squares[2 - y_coord][x_coord]
-  end
-
-  def display
-    print("  +---+---+---+\n")
-    @squares.each_with_index { |row, i| print("#{3 - i} | #{row[0].empty? ? ' ' : row[0]} | #{row[1].empty? ? ' ' : row[1]} | #{row[2].empty? ? ' ' : row[2]} |\n  +---+---+---+\n") }
-    puts('    1   2   3  ')
-  end
-
-  def full?
-    @squares.all? { |row| row.none? { |square| square == '' } }
-  end
-
-  def over?
-    flat_board = @squares.flatten
+  def over?(board = @squares)
+    flat_board = board.flatten
     WINNERS.any? do |winner|
       matcher = [flat_board[winner[0]], flat_board[winner[1]], flat_board[winner[2]]].uniq!
 
       return true if !matcher.nil? && matcher.length == 1 && matcher != ['']
     end
 
-    full?
+    full?(board)
+  end
+
+  def full?(board = @squares)
+    board.all? { |row| row.none? { |square| square == '' } }
+  end
+
+  def mark_square(x_coord, y_coord, player)
+    @squares[2 - y_coord][x_coord] = player.zero? ? 'X' : 'O'
+  end
+
+  def get_square(x_coord, y_coord, board = @squares)
+    board[2 - y_coord][x_coord]
+  end
+
+  def display
+    print("  +---+---+---+\n")
+    @squares.each_with_index { |row, i| print("#{3 - i} | #{row[0].empty? ? ' ' : row[0]} | #{row[1].empty? ? ' ' : row[1]} | #{row[2].empty? ? ' ' : row[2]} |\n  +---+---+---+\n") }
+    puts('    1   2   3  ')
   end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,1 +1,64 @@
+require_relative '../lib/board'
 
+describe Board do
+  describe '#over?' do
+    subject(:board) { Board.new }
+
+    context 'when game is won by one player' do
+      it 'returns true' do
+        won_position = [['X', '', 'O'], ['', 'X', ''], ['O', '', 'X']]
+        result = board.over?(won_position)
+        expect(result).to be true
+      end
+    end
+
+    context 'when game is not won' do
+      it 'sends query to #full?' do
+        not_won_position = [['', '', ''], ['', '', ''], ['', '', '']]
+        expect(board).to receive(:full?).with(not_won_position)
+        board.over?(not_won_position)
+      end
+    end
+  end
+  
+  describe '#full?' do
+    subject(:board) { Board.new }
+    
+    context 'when game board is full' do
+      it 'returns true' do
+        full_position = [%w[X X O], %w[O O X], %w[X X O]]
+        result = board.full?(full_position)
+        expect(result).to be true
+      end
+    end
+
+    context 'when game board is not full' do
+      it 'returns false' do
+        not_full_position = [['O', '', 'X'], ['', 'X', ''], ['O', '', 'X']]
+        result = board.full?(not_full_position)
+        expect(result).to be false
+      end
+    end
+  end
+
+  describe '#mark_square' do
+    subject(:board) { Board.new }
+
+    context 'when given coordinates and value' do
+      it 'updates proper square to new value' do
+        expect { board.mark_square(1, 1, 0) }.to change{ board.instance_variable_get(:@squares)[1][1] }.to('X')
+      end
+    end
+  end
+  
+  describe '#get_square' do
+    subject(:board) { Board.new }
+
+    context 'when given coordinates' do
+      it 'returns proper square value' do
+        squares = [%w[X X O], %w[O O X], %w[X X O]]
+        expect(board.get_square(1, 1, squares)).to eq('O')
+      end
+    end
+  end
+end
