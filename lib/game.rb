78d1b55f02b073da378ee1adbbfc50619a95097a
Ari Shtein <ashtein120@gmail.com>
2022-10-07 13:38:26 -0400

    Add Game tests for #play and #get_move

--- lib/game.rb
+++ lib/game.rb
@@ -10,26 +10,29 @@ class Game
   def play
     @board.display
     until @board.over?
+      puts "Player #{@turn + 1} (use the form 'x y'):"
       get_move
       @board.display
     end
     print_end
   end
 
-  def get_move
-    puts "Player #{@turn + 1} (use the form 'x y'):"
-    input = gets
-    return @turn if input.nil?
+  def get_move(input = gets.chomp)
+    coords = validate_input(input)
 
-    coords = input.chomp.strip.split
-    x = coords[0].to_i - 1
-    y = coords[1].to_i - 1
-    unless x.between?(0, 2) && y.between?(0, 2) && @board.get_square(x, y) == ''
+    x = coords[0].to_i - 1 if coords
+    y = coords[1].to_i - 1 if coords
+    unless input && coords && x.between?(0, 2) && y.between?(0, 2) && @board.get_square(x, y) == ''
       puts "\nInvalid input.\nPlease enter coordinates between 1 and 3 that point to an unoccupied square."
       return @turn
     end
     @board.mark_square(x, y, @turn)
     @turn = (@turn + 1) % 2
+  end
+
+  def validate_input(input)
+    nil unless input
+    input.strip.split if input.strip.match?(/[1-3]\s[1-3]/)
   end
 
   def print_end
@@ -40,6 +43,3 @@ class Game
     end
   end
 end
-
-tester = Game.new
-tester.play
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,1 +1,103 @@
+require_relative '../lib/game'
 
+describe Game do
+  describe '#play' do
+    subject(:game) { Game.new(0, board) }
+    let(:board) { instance_double(Board) }
+
+    context 'when game is over' do
+      before do
+        allow(board).to receive(:over?).and_return(true)
+      end
+
+      it 'displays the board then ends' do
+        expect(board).to receive(:display).once
+        expect(board).to receive(:over?).once
+        expect(game).to receive(:print_end).once
+        game.play
+      end
+    end
+
+    context 'when one guess is given then game ends' do
+      before do
+        allow(board).to receive(:over?).and_return(false, true)
+        allow(game).to receive(:puts).with "Player 1 (use the form 'x y'):"
+      end
+
+      it 'displays the board, then asks for a guess, then displays again, then ends' do
+        expect(board).to receive(:display).twice
+        expect(board).to receive(:over?).twice
+        expect(game).to receive(:get_move).once
+        expect(game).to receive(:print_end).once
+        game.play
+      end
+    end
+
+    context 'when 4 guesses are given then game ends' do
+      before do
+        allow(board).to receive(:over?).and_return(false, false, false, false, true)
+        allow(game).to receive(:puts).with "Player 1 (use the form 'x y'):"
+      end
+
+      it 'displays the board, then asks for a guess & displays the board 4 times, then ends' do
+        expect(board).to receive(:display).exactly(5).times
+        expect(board).to receive(:over?).exactly(5).times
+        expect(game).to receive(:get_move).exactly(4).times
+        expect(game).to receive(:print_end).once
+        game.play
+      end
+    end
+  end
+
+  describe '#get_move' do
+    subject(:game) { Game.new(0, board) }
+    let(:board) { instance_double(Board) }
+
+    context 'when given valid input' do
+      before do
+        allow(board).to receive(:get_square).and_return('')
+      end
+
+      it 'returns next turn' do
+        error_message = "\nInvalid input.\nPlease enter coordinates between 1 and 3 that point to an unoccupied square."
+        input = '3 3'
+        expect(game).to receive(:validate_input).and_return(input.split)
+        expect(board).to receive(:get_square).once
+        expect(board).to receive(:mark_square).once
+        expect(game).not_to receive(:puts).with error_message
+        result = game.get_move(input)
+        expect(result).to eq(1)
+      end
+    end
+
+    context 'when given invalid input' do
+      it 'displays error, returns same turn' do
+        error_message = "\nInvalid input.\nPlease enter coordinates between 1 and 3 that point to an unoccupied square."
+        input = 'a a'
+        expect(game).to receive(:validate_input).and_return(nil)
+        expect(board).not_to receive(:mark_square)
+        expect(game).to receive(:puts).with error_message
+        result = game.get_move(input)
+        expect(result).to eq(0)
+      end
+    end
+
+    context 'when given valid input, but square is taken' do
+      before do
+        allow(board).to receive(:get_square).and_return('X')
+      end
+      it 'displays error, returns same turn' do
+        error_message = "\nInvalid input.\nPlease enter coordinates between 1 and 3 that point to an unoccupied square."
+        input = '1 1'
+        expect(game).to receive(:validate_input).and_return(input.split)
+        expect(board).to receive(:get_square)
+        expect(board).not_to receive(:mark_square)
+        expect(game).to receive(:puts).with error_message
+        result = game.get_move(input)
+        expect(result).to eq(0)
+      end
+    end
+  end
+
+  describe '#validate_input'
+end
